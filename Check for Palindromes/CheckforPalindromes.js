f8805572498db010a2cd85e10e746c49043265a9
liuyiqi <liuyiqi@tuniu.com>
2017-10-31 17:34:09 +0800

    add another method

--- Check for Palindromes/CheckforPalindromes.js
+++ Check for Palindromes/CheckforPalindromes.js
@@ -1,5 +1,7 @@
 function palindrome(str) {
   var rep=/[^A-Za-z0-9]/g;
+  //var rep=/[\W_]/g;       /W匹配非单词字符
+
   var newstr=str.toLowerCase().replace(rep,'');
   var revstr=newstr.split("").reverse().join("");
   return newstr==revstr;
