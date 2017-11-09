37242a8e2667de3af2cd2a10a7db41ed4bfbb29a
liuyiqi <liuyiqi@tuniu.com>
2017-11-09 21:28:46 +0800

    edit where

--- Where do I belong/WheredoIbelong.js
+++ Where do I belong/WheredoIbelong.js
@@ -1,6 +1,3 @@
-/**
- * Created by liuyiqi on 2017/11/7.
- */
 function getIndexToIns(arr, num) {
     //my
     arr.push(num);
@@ -8,8 +5,7 @@ function getIndexToIns(arr, num) {
     return arr.indexOf(num);
 
     //another
-    var pos=arr.sort((a,b)=>a-b).findIndex(item=>num<=item);//findIndex()方法返回数组中满足提供的测试函数的第一个元素的索引
+    var pos=arr.sort((a,b)=>a-b).findIndex(item=>num<=item); //findIndex()方法返回数组中满足提供的测试函数的第一个元素的索引
     return pos==-1?arr.length:pos;
 }
 
-console.log(getIndexToIns([3, 10, 5],34));
